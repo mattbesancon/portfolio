a79472497c3995f4ee544019fac213951c9587e9
Matthieu Besancon <matthieu.besancon@gmail.com>
2020-12-18 17:22:35 +0100

    home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import image from '../keyboard-5017973_1920.jpg';
 
 class Home extends React.Component {
   render() {
-    return <p>Home Page</p>;
+    return (
+      <main>
+        <img
+        src={image}
+        alt='keyboard night'
+        className='absolute object-cover w-full h-full'>
+        </img>
+        <section className='relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8'>
+          <h1 className='text-6xl text-red-400 font-bold cursive leading-none lg:leading-snug home-name'>
+            Welcome there! This is Matthieu Besançon's portfolio :)
+          </h1>
+        </section>
+      </main>
+    );
   }
 }
 
 export default Home;
+
