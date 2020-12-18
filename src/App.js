a0827362c6cef9f48a0cd60c87b2f3805ef8822d
Matthieu Besancon <matthieu.besancon@gmail.com>
2020-12-18 16:41:13 +0100

    work on navbar

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import About from './components/About';
 import SinglePost from './components/SinglePost';
 import Post from './components/Post';
 import Project from './components/Project';
+import NavBar from './components/NavBar';
 
 
 function App() {
   return (
     <BrowserRouter>
+    <NavBar />
       <Switch>
         <Route exact path='/' component={Home} />
         <Route path='/about' component={About} />
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,40 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 class NavBar extends React.Component {
   render() {
-    return <p>NavBar Page</p>;
+    return (
+      <header className='bg-red-400'>
+        <div className='container mx-auto flex justify-content'>
+          <nav className='flex'>
+            <NavLink
+              exact to='/'
+              activeClassName='text-white'
+              className='inflex-flex items-center py-6 px-3 mr-4 text-red-100 hover:text-blue-600 text-4xl font-bold cursive tracking-widest'>
+              Buzz
+            </NavLink>
+            <NavLink
+              exact to='/post'
+              activeClassName='text-red-100 bg-red-500'
+              className='inflex-flex items-center py-8 px-3 mr-4 text-red-100 hover:text-blue-600 text-2xl font-bold cursive tracking-widest'>
+              Blog Posts
+            </NavLink>
+            <NavLink
+              exact to='/project'
+              activeClassName='text-red-100 bg-red-500'
+              className='inflex-flex items-center py-8 px-3 mr-4 text-red-100 hover:text-blue-600 text-2xl font-bold cursive tracking-widest'>
+              Projects
+            </NavLink>
+            <NavLink
+              exact to='/about'
+              activeClassName='text-red-100 bg-red-500'
+              className='inflex-flex items-center py-8 px-3 mr-4 text-red-100 hover:text-blue-600 text-2xl font-bold cursive tracking-widest'>
+              About Me!
+            </NavLink>
+          </nav>
+        </div>
+      </header>
+    );
   }
 }
 
