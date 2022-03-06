a3015cfb6748b8a098c3cc48ccd71d2138889038
Matthieu Besancon <matthieu.besancon@gmail.com>
2022-03-06 22:08:45 +0100

    change config for sanityClient

--- src/client.js
+++ src/client.js
@@ -2,7 +2,10 @@ import sanityClient from '@sanity/client'
 
 const client = sanityClient({
     projectId: "klaxwjmb",
-    dataset: "production"
+    dataset: "production",
+    apiVersion: '2021-03-25',
+    token: 'sanity-auth-token',
+    useCdn: true,
 });
 
 export default client;
