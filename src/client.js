a7ed97401136cb4d692547cfa942af00df8dc5ee
Matthieu Besancon <matthieu.besancon@gmail.com>
2022-03-07 21:26:51 +0100

    check if proposal in comment works

--- src/client.js
+++ src/client.js
@@ -9,3 +9,19 @@ const client = sanityClient({
 });
 
 export default client;
+
+/*
+import sanityClient from 'part:@sanity/base/client'
+
+const client = sanityClient.withConfig({ apiVersion: '2021-03-25' })
+
+client.createOrReplace({
+_id: 'translationService.secrets',
+_type: 'smartlingSettings',
+organization: 'YOUR_ORG_HERE',
+project: 'YOUR_PROJECT_HERE',
+secret: 'YOUR_TOKEN_SECRET_HERE',
+})
+
+export default client;
+*/
