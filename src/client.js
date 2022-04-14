9916626a7125cbcadf0714ec721928ad867575df
Matthieu Besancon <matthieu.besancon@gmail.com>
2022-04-14 11:11:28 +0200

    change config client & sanity upgrade & new sanity build

--- src/client.js
+++ src/client.js
@@ -4,24 +4,7 @@ const client = sanityClient({
     projectId: "klaxwjmb",
     dataset: "production",
     apiVersion: '2021-03-25',
-    token: 'sanity-auth-token',
-    useCdn: true,
+    useCdn: false,
 });
 
 export default client;
-
-/*
-import sanityClient from 'part:@sanity/base/client'
-
-const client = sanityClient.withConfig({ apiVersion: '2021-03-25' })
-
-client.createOrReplace({
-_id: 'translationService.secrets',
-_type: 'smartlingSettings',
-organization: 'YOUR_ORG_HERE',
-project: 'YOUR_PROJECT_HERE',
-secret: 'YOUR_TOKEN_SECRET_HERE',
-})
-
-export default client;
-*/
